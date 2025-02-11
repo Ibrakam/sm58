c89aafe023e25a4bb6c5a62da2a183f5df67f315
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2025-02-11 17:39:34 +0500

    dfghj

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from api.photos import photo_router
 from api.users import user_router
 from api.hashtags import hashtag_router
 from api.comments import comment_router
 from api.posts import post_router
+from db.postservice import get_all_or_exact_post
+
 from db import Base, engine
+
+templates = Jinja2Templates(directory="templates")
 # Для создания бд
 Base.metadata.create_all(engine)
 
@@ -16,4 +22,11 @@ app.include_router(post_router)
 app.include_router(comment_router)
 app.include_router(hashtag_router)
 
-# pip install email-validator
+
+@app.get("/user/{post_id}", response_class=HTMLResponse)
+async def main(request: Request, post_id: int):
+    user_post = get_all_or_exact_post(post_id)
+    return templates.TemplateResponse(name="index.html", request=request,
+                                      context={"user_post": user_post})
+
+# pip install jinja2
